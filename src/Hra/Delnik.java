000671f9aae4fbcd93a9a7ec1f5b41cccd8cf9f9
tompa <tompavelek23@gmail.com>
2026-05-19 11:25:16 +0200

    pozadi delnika pridano

--- src/Hra/Delnik.java
+++ src/Hra/Delnik.java
@@ -10,22 +10,36 @@ public class Delnik extends JLabel {
     private boolean doprava = true;
 
     public Delnik(int poziceX, int poziceY) {
-        this.setOpaque(true);
-        this.setBackground(Color.YELLOW);
+        this.setOpaque(false);
         this.setBounds(poziceX, poziceY, 50, 50);
+        this.setIcon(hotovaIkona);
     }
+
+    ImageIcon puvodniIkona = new ImageIcon(getClass().getResource("/delnikDoprava.png"));
+    Image surovyObrazek = puvodniIkona.getImage();
+    Image zmensenyObrazek = surovyObrazek.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+    ImageIcon hotovaIkona = new ImageIcon(zmensenyObrazek);
+
+    ImageIcon puvodniIkona2 = new ImageIcon(getClass().getResource("/delnikDoleva.png"));
+    Image surovyObrazek2 = puvodniIkona2.getImage();
+    Image zmensenyObrazek2 = surovyObrazek2.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+    ImageIcon hotovaIkona2 = new ImageIcon(zmensenyObrazek2);
+
+
 
     public void posun(){
         int x = this.getX();
         int y = this.getY();
         if (doprava){
             this.setLocation(x + rychlost, y);
-            if (this.getX() >= 1100){
+            if (this.getX() >= 1120){
+                this.setIcon(hotovaIkona2);
                 doprava = false;
             }
         } else {
             this.setLocation(x - rychlost, y);
             if (this.getX() <= 300){
+                this.setIcon(hotovaIkona);
                 doprava = true;
             }
         }
