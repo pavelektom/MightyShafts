43d7697118e394ada5e759ae65d17a2a52ae2360
tompa <tompavelek23@gmail.com>
2026-05-27 20:20:00 +0200

    tezeni + pohyb

--- src/Hra/Delnik.java
+++ src/Hra/Delnik.java
@@ -6,7 +6,16 @@ import java.awt.*;
 
 public class Delnik extends JLabel {
 
+    StavDelnika aktualniStav = StavDelnika.JDE_TEZIT;
+
+    int vylozenaRuda =0;
+    private int casTezby;
+    private int kapacita = 10;
+    private int batoh = 0;
+    private int level = 1;
     private int rychlost = 3;
+
+
     private boolean doprava = true;
 
     public Delnik(int poziceX, int poziceY) {
@@ -24,25 +33,42 @@ public class Delnik extends JLabel {
     Image zmensenyObrazek2 = obrazek2.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
     ImageIcon hotovej2 = new ImageIcon(zmensenyObrazek2);
 
-
-
-    public void posun(){
+    public void posun() {
         int x = this.getX();
         int y = this.getY();
-        if (doprava){
+
+        if (aktualniStav == StavDelnika.JDE_TEZIT) {
             this.setLocation(x + rychlost, y);
-            if (this.getX() >= 1120){
-                this.setIcon(hotovej2);
-                doprava = false;
-            }
-        } else {
-            this.setLocation(x - rychlost, y);
-            if (this.getX() <= 300){
-                this.setIcon(hotovej);
-                doprava = true;
+
+            if (this.getX() >= 1120) {
+                aktualniStav = StavDelnika.TEZI;
             }
         }
+        else if (aktualniStav == StavDelnika.TEZI) {
+            casTezby++;
+            if (casTezby >= 50) {
+                batoh += level;
+                casTezby = 0;
+            }
+            if (batoh >= kapacita) {
+                aktualniStav = StavDelnika.JDE_K_VYTAHU;
+                this.setIcon(hotovej2);
 
+            }
+        }
+        else if (aktualniStav == StavDelnika.JDE_K_VYTAHU) {
+            this.setLocation(x - rychlost, y);
+
+            if (this.getX() <= 300) {
+                aktualniStav = StavDelnika.VYKLADA;
+                this.setIcon(hotovej);
+            }
+        }
+        else if (aktualniStav == StavDelnika.VYKLADA) {
+            vylozenaRuda += batoh;
+            batoh = 0;
+            aktualniStav = StavDelnika.JDE_TEZIT;
+        }
     }
 
 
