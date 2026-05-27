b19dfce98e214154282bcf5525073aa7ce9ddf08
tompa <tompavelek23@gmail.com>
2026-05-27 20:14:40 +0200

    pridani kolik je v sachte

--- src/Hra/Game.java
+++ src/Hra/Game.java
@@ -10,8 +10,11 @@ public class Game {
     private JFrame frame;
     private Image obrazekPozadi1;
 
-    private static int cash = 0;
+
+    static int cash = 0;
     private JLabel penizeText;
+
+    public static Vytah vytah;
 
     public Game(){
         frame = new JFrame();
@@ -26,9 +29,45 @@ public class Game {
         frame.setLayout(new BorderLayout());
 
         penizeText = new JLabel("Cash: " + "$");
-        penizeText.setBounds(1050, 5, 250, 30);
+        penizeText.setBounds(1050, 5, 650, 30);
         penizeText.setFont(new Font("Arial", Font.BOLD, 40));
         penizeText.setForeground(Color.GREEN);
+
+        JLabel textTruhly = new JLabel("");
+        textTruhly.setBounds(200, 60, 6200, 40);
+        textTruhly.setFont(new Font("Arial", Font.BOLD, 35));
+        textTruhly.setForeground(Color.ORANGE);
+        frame.add(textTruhly);
+
+        JLabel ruda1 = new JLabel("");
+        ruda1.setBounds(300, 440, 100, 30);
+        ruda1.setFont(new Font("Arial", Font.BOLD, 22));
+        ruda1.setForeground(Color.YELLOW);
+        frame.add(ruda1);
+
+        JLabel ruda2 = new JLabel("");
+        ruda2.setBounds(300, 590, 100, 30);
+        ruda2.setFont(new Font("Arial", Font.BOLD, 22));
+        ruda2.setForeground(Color.YELLOW);
+        frame.add(ruda2);
+
+        JLabel ruda3 = new JLabel("");
+        ruda3.setBounds(300, 705, 100, 30);
+        ruda3.setFont(new Font("Arial", Font.BOLD, 22));
+        ruda3.setForeground(Color.YELLOW);
+        frame.add(ruda3);
+
+        JLabel ruda4 = new JLabel("");
+        ruda4.setBounds(300, 815, 100, 30);
+        ruda4.setFont(new Font("Arial", Font.BOLD, 22));
+        ruda4.setForeground(Color.YELLOW);
+        frame.add(ruda4);
+
+        JLabel ruda5 = new JLabel("");
+        ruda5.setBounds(300, 970, 100, 30);
+        ruda5.setFont(new Font("Arial", Font.BOLD, 22));
+        ruda5.setForeground(Color.YELLOW);
+        frame.add(ruda5);
 
 
 
@@ -69,9 +108,10 @@ public class Game {
         Delnik delnik3 = new Delnik(300, 695);
         Delnik delnik4 = new Delnik(300, 806);
         Delnik delnik5 = new Delnik(300, 960);
+        Delnik[] poledelniku = {delnik1, delnik2, delnik3, delnik4, delnik5};
 
-        Vytah vytah = new Vytah(170, 310);
-        Skladnik skladnik = new Skladnik(315, 235);
+        vytah = new Vytah(170, 310, poledelniku);
+        Skladnik skladnik = new Skladnik(1160, 235);
 
         frame.add(skladnik);
         frame.add(vytah);
@@ -89,7 +129,39 @@ public class Game {
                 delnik5.posun();
                 vytah.posun();
                 skladnik.posun();
-                penizeText.setText("Cash: " + cash + " $");
+
+                penizeText.setText("Peníze: " + cash + " $ ");
+
+                if (Vytah.truhlaNahore > 0) {
+                    textTruhly.setText(String.valueOf(Vytah.truhlaNahore));
+                } else {
+                    textTruhly.setText("");
+                }
+                if (delnik1.vylozenaRuda > 0) {
+                    ruda1.setText(String.valueOf(delnik1.vylozenaRuda));
+                } else {
+                    ruda1.setText("");
+                }
+                if (delnik2.vylozenaRuda > 0) {
+                    ruda2.setText(String.valueOf(delnik2.vylozenaRuda));
+                } else {
+                    ruda2.setText("");
+                }
+                if (delnik3.vylozenaRuda > 0) {
+                    ruda3.setText(String.valueOf(delnik3.vylozenaRuda));
+                } else {
+                    ruda3.setText("");
+                }
+                if (delnik4.vylozenaRuda > 0) {
+                    ruda4.setText(String.valueOf(delnik4.vylozenaRuda));
+                } else {
+                    ruda4.setText("");
+                }
+                if (delnik5.vylozenaRuda > 0) {
+                    ruda5.setText(String.valueOf(delnik5.vylozenaRuda));
+                } else {
+                    ruda5.setText("");
+                }
             });
 
         cas.start();
