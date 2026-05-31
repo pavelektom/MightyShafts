66bbf720d9a0c76ebcd759a8ea4d393a30bd63ae
tompa <tompavelek23@gmail.com>
2026-05-31 21:00:29 +0200

    maličkost

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Music m = new Music("resources/Below_the_Bedrock.wav");
-        Thread abc = new Thread(m);
-        abc.setPriority(Thread.MIN_PRIORITY);
-
         m.run();
         new MainMenu().zapnout();
     }
